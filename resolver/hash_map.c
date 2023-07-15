a35eacc009b866477ac4ee0604efb57ec9969541
Jan Prukner <prukner.jan@seznam.cz>
2023-07-15 21:06:56 +0200

    deal with collisions using quadratic stepping

--- resolver/hash_map.c
+++ resolver/hash_map.c
@@ -3,15 +3,24 @@
 #include <string.h>
 #include <sys/types.h>
 
+struct hash_map_meta_value {
+  const char *key;
+  uint32_t value;
+};
+
 struct hash_map {
-  int *array;
+  struct hash_map_meta_value *array;
   uint64_t size;
 };
 
 struct hash_map *hash_map_new(uint64_t prime_number_size) {
   struct hash_map *map = malloc(sizeof(struct hash_map));
   map->size = prime_number_size;
-  map->array = malloc(sizeof(int) * prime_number_size);
+  map->array = malloc(sizeof(struct hash_map_meta_value) * prime_number_size);
+  for (int i = 0; i < prime_number_size; i++) {
+    struct hash_map_meta_value value = {.value = 0, .key = NULL};
+    map->array[i] = value;
+  }
   return map;
 }
 
@@ -21,6 +30,8 @@ void hash_map_free(struct hash_map *map) {
 }
 
 #define HASH_BYTES_STEP 8
+
+enum lookup_error { NO_ERROR, ERR_KEY_NOT_FOUND };
 
 // hash returns hash value of given value.
 static uint64_t hash(const char *key) {
@@ -42,14 +53,42 @@ static uint64_t hash(const char *key) {
   return result;
 }
 
-void hash_map_add(struct hash_map *map, const char *key, int value) {
+void hash_map_add(struct hash_map *map, const char *key, uint32_t value) {
   uint64_t hashed_key = hash(key);
   uint64_t position = hashed_key % map->size;
-  map->array[position] = value;
+  struct hash_map_meta_value stored_value;
+  int32_t quadratic_steps = 0;
+  stored_value = map->array[position];
+  while (stored_value.key != NULL) {
+    position += 1 << quadratic_steps;
+    position = position % map->size;
+    stored_value = map->array[position];
+    quadratic_steps++;
+  }
+  map->array[position] =
+      (struct hash_map_meta_value){.key = key, .value = value};
 }
 
-int hash_map_get(struct hash_map *map, const char *key) {
+uint32_t hash_map_get(struct hash_map *map, const char *key,
+                      enum lookup_error *error) {
   uint64_t hashed_key = hash(key);
   uint64_t position = hashed_key % map->size;
-  return map->array[position];
+  struct hash_map_meta_value stored_value;
+  stored_value = map->array[position];
+  int32_t quadratic_steps = 0;
+  while (1) {
+    if (stored_value.key == NULL) {
+      *error = ERR_KEY_NOT_FOUND;
+      return 0;
+    }
+    if (strcmp(stored_value.key, key) == 0) {
+      break;
+    }
+    position += 1 << quadratic_steps;
+    position = position % map->size;
+    stored_value = map->array[position];
+    quadratic_steps++;
+  }
+  *error = NO_ERROR;
+  return stored_value.value;
 }
--- resolver/hash_map_test.c
+++ resolver/hash_map_test.c
@@ -3,24 +3,35 @@
 #include <string.h>
 
 int main() {
+  enum lookup_error error;
+
   struct hash_map *map = hash_map_new(31);
   hash_map_add(map, "new", 42);
-  int value = hash_map_get(map, "new");
+  int value = hash_map_get(map, "new", &error);
+  assert(error == NO_ERROR);
   assert(value == 42);
 
   hash_map_add(map, "key", 51);
-  value = hash_map_get(map, "key");
+  value = hash_map_get(map, "key", &error);
+  assert(error == NO_ERROR);
   assert(value == 51);
 
   hash_map_add(map, "kez", 50);
-  value = hash_map_get(map, "kez");
+  value = hash_map_get(map, "kez", &error);
+  assert(error == NO_ERROR);
   assert(value == 50);
 
   hash_map_add(map, "kez8000asdff", 82);
-  value = hash_map_get(map, "kez8000asdff");
+  value = hash_map_get(map, "kez8000asdff", &error);
+  assert(error == NO_ERROR);
   assert(value == 82);
 
   hash_map_add(map, "kez8da00asdff", 500);
-  value = hash_map_get(map, "kez8da00asdff");
+  value = hash_map_get(map, "kez8da00asdff", &error);
+  assert(error == NO_ERROR);
   assert(value == 500);
+
+  value = hash_map_get(map, "NOKEYLIKETHIS", &error);
+  assert(error == ERR_KEY_NOT_FOUND);
+  assert(value == 0);
 }
