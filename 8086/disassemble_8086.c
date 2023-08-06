3980fc731b39673564554b20ab9349e3827b4159
Jan Prukner <prukner.jan@seznam.cz>
2023-08-06 18:41:42 +0200

    add ability to disassemble mov only in Register mode

--- 8086/disassemble_8086.c
+++ 8086/disassemble_8086.c
@@ -1,6 +1,7 @@
 // main takes one argument and that is a path to binary file of 8086 executable.
 // It prints out assembly instructions in a format that can be again fed to nasm
 // assembler.
+#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,41 +22,101 @@ void print_byte_as_binary(unsigned char input) {
     bit_position = bit_position - 1;
   }
   output[sizeof_input_bits] = '\0';
-  printf("%s", output);
+  fprintf(stderr, "%s", output);
   return;
 }
 
-void print_bytes_as_binary(const unsigned char bytes[], uint64_t count) {
-  for (uint64_t i = 0; i < count; i++) {
-    print_byte_as_binary(bytes[i]);
-    printf(" ");
-  }
-  printf("\n");
+void debug_byte_as_binary(const char *message, unsigned char byte) {
+  fprintf(stderr, message);
+  print_byte_as_binary(byte);
+  fprintf(stderr, "\n");
 }
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
-    printf(
+    fprintf(
+        stderr,
         "%s expects exactly one 8086 executable path %d arguments were given\n",
         argv[0], argc - 1);
     return EXIT_FAILURE;
   }
-  printf("disassembling file %s\n", argv[1]);
+  fprintf(stderr, "disassembling file %s\n", argv[1]);
   FILE *executable = fopen(argv[1], "rb");
-  unsigned char instruction[2];
-  size_t n = fread(&instruction, sizeof(instruction), 1, executable);
-  if (n != 1) {
-    printf(
-        "we expected to read one instruction but %lu instructions were read\n",
-        n);
-    goto exit_failure;
-  }
-  print_bytes_as_binary(instruction, 2);
+  unsigned char instruction_byte;
+  // TODO MAKE THIS CONSTANTST
+  const unsigned char opcode_mask = 0xfc;
+  const unsigned char direction_mask = 0x02;
+  const unsigned char word_mask = 0x01;
+  const unsigned char mode_mask = 0xc0;
+  const unsigned char register_mask = 0x38;
+  const unsigned char register_memory_mask = 0x07;
 
-exit_success:
+  char *register_word_map[][8] = {
+      {"al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"},
+      {"ax", "cx", "dx", "bx", "sp", "bp", "si", "di"},
+  };
+
+  int exit_code = EXIT_SUCCESS;
+  printf("bits 16\n\n");
+  while (fread(&instruction_byte, sizeof(instruction_byte), 1, executable) ==
+         1) {
+
+    debug_byte_as_binary("first instruction byte as binary:", instruction_byte);
+
+    unsigned char opcode = instruction_byte & opcode_mask;
+    unsigned char direction = (instruction_byte & direction_mask) >> 1;
+    unsigned char word = instruction_byte & word_mask;
+    debug_byte_as_binary("opcode as binary: ", opcode);
+
+    switch (opcode) {
+    case 0x88:
+      fprintf(stderr, "it's mov type 'Register/memory to/from register' \n");
+      size_t n =
+          fread(&instruction_byte, sizeof(instruction_byte), 1, executable);
+      if (n != 1) {
+        fprintf(
+            stderr,
+            "expected one byte for mov instruction to be complete, got none\n");
+        exit_code = EXIT_FAILURE;
+        goto exit;
+      }
+      debug_byte_as_binary("mov - next byte as binary: ", instruction_byte);
+      fprintf(stderr, "direction is %u\n", direction);
+      fprintf(stderr, "word is %u\n", word);
+
+      unsigned char mode = instruction_byte & mode_mask;
+      switch (mode) {
+      case 0xc0:
+        fprintf(stderr, "mov mode: Register Mode\n");
+        unsigned char reg = (instruction_byte & register_mask) >> 3;
+        unsigned char reg_mem = instruction_byte & register_memory_mask;
+        debug_byte_as_binary("reg:", reg);
+        debug_byte_as_binary("reg/mem:", reg_mem);
+        if (direction == 0) {
+          unsigned char tmp = reg;
+          reg = reg_mem;
+          reg_mem = tmp;
+        }
+
+        printf("mov %s, %s\n", register_word_map[word][reg],
+               register_word_map[word][reg_mem]);
+
+        break;
+      default:
+        debug_byte_as_binary("unknown mode: ", mode);
+        exit_code = EXIT_FAILURE;
+        goto exit;
+      }
+      break;
+    default:
+      debug_byte_as_binary("unknown opcode: ", opcode);
+      exit_code = EXIT_FAILURE;
+      goto exit;
+    };
+    fprintf(stderr, "---- next instruction ----\n");
+  }
+
+exit:
   fclose(executable);
-  return EXIT_SUCCESS;
-exit_failure:
-  fclose(executable);
-  return EXIT_FAILURE;
+  return exit_code;
 }
