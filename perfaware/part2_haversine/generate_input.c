78939cb1cc0dbae6aff0defcbb5f73e4663a43ec
Jan Prukner <prukner.jan@seznam.cz>
2023-09-18 11:06:23 +0200

    add command line options that feed points generation

--- perfaware/part2_haversine/generate_input.c
+++ perfaware/part2_haversine/generate_input.c
@@ -1,8 +1,6 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define CLUSTER_COUNT 2
-#define NUMBER_OF_POINTS_IN_CLUSTER 20
 #define CLUSTER_RANGE 60
 
 int16_t random_in_range(int16_t lower, int16_t upper) {
@@ -10,20 +8,37 @@ int16_t random_in_range(int16_t lower, int16_t upper) {
 }
 
 int main(int argc, char *argv[]) {
-  for (int n = 0; n < CLUSTER_COUNT; ++n) {
+
+  if (argc < 3) {
+    fprintf(stderr,
+            "expected exactly 2 arguments %d given: cluster count and point "
+            "count per cluster expected\n",
+            argc);
+    return EXIT_FAILURE;
+  }
+
+  // TODO check for errors;
+  int cluster_count = atoi(argv[1]);
+  int points_per_cluster = atoi(argv[2]);
+  fprintf(stderr, "cluster_count: %d\npoints_per_cluster:%d\n", cluster_count,
+          points_per_cluster);
+
+  for (int n = 0; n < cluster_count; ++n) {
+
     uint64_t points_count = 0;
     int16_t cluster_centre_x = random_in_range(-180, 180);
     int16_t cluster_centre_y = random_in_range(-90, 90);
-    printf("----cluster %d with centre [\"x\":%d, \"y\"%d]-----\n", n,
-           cluster_centre_x, cluster_centre_y);
-    while (points_count < NUMBER_OF_POINTS_IN_CLUSTER) {
+    fprintf(stderr, "----cluster %d with centre [\"x\":%d, \"y\"%d]-----\n", n,
+            cluster_centre_x, cluster_centre_y);
+    while (points_count < points_per_cluster) {
       int16_t new_point_x_start = cluster_centre_x - (CLUSTER_RANGE / 2);
+
       if (new_point_x_start < -180) {
-        new_point_x_start = (new_point_x_start + 180) + 180;
+        new_point_x_start = -180;
       }
       int16_t new_point_x_stop = cluster_centre_x + (CLUSTER_RANGE / 2);
       if (new_point_x_stop > 180) {
-        new_point_x_stop = (new_point_x_stop - 180) - 180;
+        new_point_x_stop = 180;
       }
 
       int16_t new_point_y_start = cluster_centre_y - (CLUSTER_RANGE / 2);
