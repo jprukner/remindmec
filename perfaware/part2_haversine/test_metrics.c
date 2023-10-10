5abfaa9b1476fff2b4b4053f2a25c88486b6aa39
Jan Prukner <prukner.jan@seznam.cz>
2023-10-10 14:48:26 +0200

    Ensure timings are correct for recursive blocks

--- perfaware/part2_haversine/test_metrics.c
+++ perfaware/part2_haversine/test_metrics.c
@@ -1,5 +1,14 @@
 #define DEBUG_TIMERS_COUNT 100
 #include "metrics.h"
+
+int multiplyNumbers(int n) {
+  TIME_FUNCTION;
+  if (n >= 1)
+    return n * multiplyNumbers(n - 1);
+  else
+    RETURN(1);
+}
+
 void foo() {
   TIME_FUNCTION;
   int a = 1000;
@@ -14,6 +23,27 @@ void foo() {
     }
     END_TIMER();
   }
+  {
+    TIME_BLOCK("d: while 1000 counts down to 0");
+    int b = 1000;
+    while (b != 0) {
+      b--;
+    }
+    END_TIMER();
+  }
+  RETURN_NOTHING();
+}
+
+void recursive_int(int count) {
+  TIME_FUNCTION;
+  if (count <= 0) {
+    RETURN_NOTHING();
+  }
+  int a = 100;
+  while (a != 0) {
+    a--;
+  }
+  recursive_int(count - 1);
   RETURN_NOTHING();
 }
 
@@ -31,5 +61,6 @@ int main() {
     END_TIMER();
   }
   foo();
+  recursive_int(5);
   PRINT_TIMINGS();
 }
