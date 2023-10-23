d3be5c4aa3f2e7feec334dc966fd508dc34c0638
Jan Prukner <prukner.jan@seznam.cz>
2023-10-23 08:39:09 +0200

    add fragment shader

--- opengl/window.c
+++ opengl/window.c
@@ -37,6 +37,57 @@ int main() {
 		perror("failed to init glad:");
 		return 1;
 	}
+
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f
+	};
+
+	uint32_t VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	const char *vertexShaderSource = "#version 330 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main()\n"
+		"{\n"
+		"  gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\0";
+
+	uint32_t vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	int success;
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if(!success){
+		char infoLog[512];
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		fprintf(stderr, "failed to compile vertex shader: %s\n", infoLog);
+		return 1;
+	}
+
+	const char *fragmentShaderSource = "#version 330 core\n"
+		"out vec4 color;\n"
+		"void main()\n"
+		"{\n"
+		"  color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n";
+		"}\0";
+	uint32_t fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	if(!success){
+                char infoLog[512];
+                glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+                fprintf(stderr, "failed to compile fragment shader: %s\n", infoLog);
+                return 1;
+        }
+
+	// todo shader program
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
    while (!glfwWindowShouldClose(window))
     {
 	processInput(window);
