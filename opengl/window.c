f30a909a6f36ce4ee51b2d26b5d9d9986885ae69
Jan Prukner <prukner.jan@seznam.cz>
2023-10-23 09:08:34 +0200

    draw triangle

--- opengl/window.c
+++ opengl/window.c
@@ -44,10 +44,16 @@ int main() {
 		0.0f, 0.5f, 0.0f
 	};
 
-	uint32_t VBO;
+	uint32_t VBO, VAO;
+	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 
 	const char *vertexShaderSource = "#version 330 core\n"
 		"layout (location = 0) in vec3 aPos;\n"
@@ -72,11 +78,12 @@ int main() {
 		"out vec4 color;\n"
 		"void main()\n"
 		"{\n"
-		"  color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n";
-		"}\0";
+		"  color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"}\n\0";
 	uint32_t fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
 	glCompileShader(fragmentShader);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
 	if(!success){
                 char infoLog[512];
                 glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
@@ -84,9 +91,21 @@ int main() {
                 return 1;
         }
 
-	// todo shader program
+	uint32_t shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if(!success) {
+		char infoLog[512];
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		fprintf(stderr, "failed to link program: %s\n", infoLog);
+		return 1;
+	}
+
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
+
 
    while (!glfwWindowShouldClose(window))
     {
@@ -94,6 +113,10 @@ int main() {
         // ------
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+	// -- draw a triangle
+	glUseProgram(shaderProgram);
+	glBindVertexArray(VAO);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 	// ----
         glfwSwapBuffers(window);
         glfwPollEvents();
