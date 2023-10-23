2b13236f78813ed9b71a4d93a42ed15d361bf0bf
Jan Prukner <prukner.jan@seznam.cz>
2023-10-23 10:41:57 +0200

    use draw elements to draw rectangles

--- opengl/window.c
+++ opengl/window.c
@@ -39,17 +39,29 @@ int main() {
 	}
 
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f
+		-0.5f, 0.5f, 0.0f,  // top left
+		 0.5f, 0.5f, 0.0f,   // top right
+		-0.5f, -0.5f, 0.0f, // bottom left
+		0.5f, -0.5f, 0.0f   // bottom right
 	};
 
-	uint32_t VBO, VAO;
+	unsigned int indecies[] = {
+		0, 1, 2,
+		1, 2, 3
+	};
+
+	uint32_t VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 	glBindVertexArray(VAO);
+
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indecies), indecies, GL_STATIC_DRAW);
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -106,6 +118,7 @@ int main() {
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
    while (!glfwWindowShouldClose(window))
     {
@@ -116,7 +129,8 @@ int main() {
 	// -- draw a triangle
 	glUseProgram(shaderProgram);
 	glBindVertexArray(VAO);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
 	// ----
         glfwSwapBuffers(window);
         glfwPollEvents();
